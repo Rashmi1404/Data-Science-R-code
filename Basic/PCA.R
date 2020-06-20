52c1702ce4ede602efb2ccd3a27dd11d61e3340c
Rashmi1404 <60732492+Rashmi1404@users.noreply.github.com>
2020-06-20 15:47:54 +0530

    Updated

--- Basic/PCA.R
+++
@@ -1,9 +0,0 @@
-PCA<-read.csv("/Volumes/Data/Course Content/DS content/PCA/PCA.csv")
-
-pca<-princomp(PCA[,2:7], cor = TRUE,scores = TRUE, covmat = NULL)
-summary(pca)
-pca$scores
-pca$loadings
-
-plot(pca$scores[,1:2],col="Blue",pch=18,cex = 0.3, lwd = 3)
-text(pca$scores[,1:2], labels=c(1:25), cex= 1)
